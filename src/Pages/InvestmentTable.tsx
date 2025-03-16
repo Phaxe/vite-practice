e24feaf361bdfe357eb12300fcd47d0dab10eefe
Phaxe <Yahia.eid@outlook.com>
2025-03-17 01:31:02 +0200

    better error handling for the table

--- src/Pages/InvestmentTable.tsx
+++ src/Pages/InvestmentTable.tsx
@@ -49,30 +49,41 @@ import { useTranslation } from "react-i18next";
 
             <TableBody>
               {/* Show Skeletons while loading */}
-              {isLoading
-                ? [...Array(5)].map((_, i) => (
-                    <TableRow key={i}>
-                      <TableCell className="p-4"><Skeleton className="h-6 w-32" /></TableCell>
-                      <TableCell className="p-4"><Skeleton className="h-6 w-20" /></TableCell>
-                      <TableCell className="p-4"><Skeleton className="h-6 w-16" /></TableCell>
-                    </TableRow>
-                  ))
-                : investments.map((investment: Investment) => (
-                    <TableRow key={investment.id} className="hover:bg-gray-50 dark:hover:bg-gray-900">
-                      <TableCell className="p-4 font-medium">{investment.name}</TableCell>
-                      <TableCell className="p-4">${investment.amount}</TableCell>
-                      <TableCell className="p-4">{investment.roi}%</TableCell>
-                      <TableCell className="p-4">
-                      <Button
-                        variant="ghost"
-                        size="icon"
-                        onClick={() => handleDelete(String(investment.id))}
-                      >
-                        <Trash2 className="w-5 h-5 text-red-500 hover:text-red-700" />
-                      </Button>
-                    </TableCell>
-                    </TableRow>
-                  ))}
+              {isLoading ? (
+              [...Array(5)].map((_, i) => (
+                <TableRow key={i}>
+                  <TableCell className="p-4">
+                    <Skeleton className="h-6 w-32" />
+                  </TableCell>
+                  <TableCell className="p-4">
+                    <Skeleton className="h-6 w-20" />
+                  </TableCell>
+                  <TableCell className="p-4">
+                    <Skeleton className="h-6 w-16" />
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : investments?.length > 0 ? (
+              investments.map((investment: Investment) => (
+                <TableRow key={investment.id} className="hover:bg-gray-50 dark:hover:bg-gray-900">
+                  <TableCell className="p-4 font-medium">{investment.name}</TableCell>
+                  <TableCell className="p-4">${investment.amount}</TableCell>
+                  <TableCell className="p-4">{investment.roi}%</TableCell>
+                  <TableCell className="p-4">
+                    <Button variant="ghost" size="icon" onClick={() => handleDelete(String(investment.id))}>
+                      <Trash2 className="w-5 h-5 text-red-500 hover:text-red-700" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              // If no investments, show message
+              <TableRow>
+                <TableCell colSpan={4} className="p-4 text-center text-gray-500">
+                  {t("no_data_available")}
+                </TableCell>
+              </TableRow>
+            )}
             </TableBody>
           </Table>
         </div>
